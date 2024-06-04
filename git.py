2d581cf45f3ff341bbdf866c7e4d353e4bdf23f7
suneeshmourya <167665604+suneeshmourya@users.noreply.github.com>
2024-06-04 11:59:38 +0530

    Add files via upload

---
+++ git.py
@@ -0,0 +1,16 @@
+
+a=int(input("Enter the value of a:"))
+opr=input("Enter the operator:")
+b=int(input("Enter the value of b:"))
+if (opr =="+"):
+    print("the sum of the two number:",a+b)
+elif(opr =="-"):
+       print("the subtract  of the two number:",a-b)
+elif(opr =="*"):
+      print("the multiply of the two number:",a*b)
+
+elif(opr =="/"):
+      print("the divison of the two number:",a/b)
+else:
+      print("invalid")
+
